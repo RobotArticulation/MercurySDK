fa8b6a2db74d87000485594264e420306c3f540d
RobotArticulation <wigir@robotarticulation.com>
2020-07-17 13:10:01 +0200

    #6: adding mercury_sdk library

---
+++ c++/include/mercury_sdk/mercury_sdk.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDE_MERCURY_SDK_MERCURYSDK_H_
+#define INCLUDE_MERCURY_SDK_MERCURYSDK_H_
+
+#include "packet_handler.h"
+#include "port_handler.h"
+
+#endif /* INCLUDE_MERCURY_SDK_MERCURYSDK_H_ */	
